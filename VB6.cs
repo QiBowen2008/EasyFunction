fe46b1fe15a6fee708da2b7c7db4b44c96c2ec4b
博文哥哥 <qibowen7852008@qq.com>
2023-03-25 15:28:09 +0800

    Add files via upload

---
+++ VB6.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyFuncation
+{
+    public static  class VB6
+    {
+        public static bool CBool(object a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(string  a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(int a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(Single  a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(double  a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(byte  a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(char a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static bool CBool(long a)
+        {
+            return Convert.ToBoolean(a);
+        }
+        public static byte CByte(bool a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(char a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(string a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(double a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(int a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(long a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static byte CByte(Single a)
+        {
+            return Convert.ToByte(a);
+        }
+        public static double CDbl(object a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(string  a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(long a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(Single  a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(byte  a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(double  a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(int a)
+        {
+            return Convert.ToDouble(a);
+        }
+        public static double CDbl(bool a)
+        {
+            return Convert.ToDouble(a);
+        }
+    }
+}
